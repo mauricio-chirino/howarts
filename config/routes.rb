acb090be6abb1648dfbfeb78a0ab29282c8a1fd2
mauricio-chirino <mchirino@aniracloud.com>
2024-09-26 11:02:57 -0400

    agregados render buils y yaml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "render/index"
   get "pages/index"
   resources :characters
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
